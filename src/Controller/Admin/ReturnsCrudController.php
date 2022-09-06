ad0e7448f0217399c20408951eca9a5d013edb1e
Al Zee <z@alz.ee>
2022-09-06 23:29:55 +0800

    returns admin crud configure fields

--- src/Controller/Admin/ReturnsCrudController.php
+++ src/Controller/Admin/ReturnsCrudController.php
@@ -4,6 +4,14 @@ namespace App\Controller\Admin;
 
 use App\Entity\Returns;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TelephoneField;
 
 class ReturnsCrudController extends AbstractCrudController
 {
@@ -12,14 +20,17 @@ class ReturnsCrudController extends AbstractCrudController
         return Returns::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->onlyOnIndex(),
+            AssociationField::new('sender'),
+            AssociationField::new('recipient'),
+            AssociationField::new('product'),
+            IntegerField::new('quantity'),
+            IntegerField::new('amount'),
+            IntegerField::new('voucher'),
+            ChoiceField::new('status')->setChoices(['Pending' => 0, 'Success' => 5]),
         ];
     }
-    */
 }
