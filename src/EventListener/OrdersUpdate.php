4ed4fac95c2a8ae0e5842d3cd4d36d01ddf2f854
Al Zee <z@alz.ee>
2022-09-06 19:08:41 +0800

    event when orders status update

--- src/EventListener/OrdersUpdate.php
+++ src/EventListener/OrdersUpdate.php
@@ -23,28 +23,30 @@ class OrdersUpdate
         $status = $order->getStatus();
 
         if (isset($changeSet['status'])) {
-        dump($status);
-            if ($status == 5) {
-                $product = $order->getProduct();
-                $sn = $product->getSn();
+            if ($status != 6) {
+                $seller_product = $order->getProduct();
+                $sn = $seller_product->getSn();
                 $seller = $order->getSeller();
                 $buyer = $order->getBuyer();
                 $quantity = $order->getQuantity();
                 $voucher = $order->getVoucher();
                 // seller stock - quantity
+                $seller_product->setStock($seller_product->getStock() - $quantity);
                 // if not find this product in buyer org, create it
                 $buyer_product = $em->getRepository(Product::class)->findOneByOrgAndSN($buyer, $sn);
+                if (is_null($buyer_product)) {
+                    $buyer_product = clone $seller_product;
+                    $buyer_product->setStock(0);
+                    $buyer_product->setOrg($buyer);
+                    $em->persist($buyer_product);
+                }
+                // buyer stock + quantity
+                $buyer_product->setStock($buyer_product->getStock() + $quantity);
+                // buyer voucher + voucher
+                // $buyer_product->setVoucher($buyer->getVoucher() + $voucher);
+                $em->flush();
+                dump($seller_product);
                 dump($buyer_product);
-                // if ($buyer_product) {
-                //     // $p = new Product();
-                //     $p = clone $product;
-                //     $p->setStock(0);
-                //     $p->setOrg($buyer);
-                // }
-                // // buyer stock + quantity
-                // // buyer voucher + voucher
-                // $em->persist($buyer_product);
-                // $em->flush();
             }
         }
 
