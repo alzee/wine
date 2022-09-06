c1c4efe3089606d4cfeaf67825b60f51b0be6445
Al Zee <z@alz.ee>
2022-09-06 16:56:29 +0800

    org crud

--- src/Controller/Admin/OrgCrudController.php
+++ src/Controller/Admin/OrgCrudController.php
@@ -4,6 +4,14 @@ namespace App\Controller\Admin;
 
 use App\Entity\Org;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TelephoneField;
 
 class OrgCrudController extends AbstractCrudController
 {
@@ -12,14 +20,16 @@ class OrgCrudController extends AbstractCrudController
         return Org::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->onlyOnIndex(),
+            TextField::new('name'),
+            TextField::new('contact'),
+            TelephoneField::new('phone'),
+            TextField::new('address'),
+            TextField::new('district'),
+            ChoiceField::new('type')->setChoices(['代理商' => 1,'门店' => 2]),
         ];
     }
-    */
 }
