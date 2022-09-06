f59b85875ad55dc426f48f3e64944449e49023b5
Al Zee <z@alz.ee>
2022-09-07 00:54:39 +0800

    voucher crud configure field

--- src/Controller/Admin/VoucherCrudController.php
+++ src/Controller/Admin/VoucherCrudController.php
@@ -4,6 +4,16 @@ namespace App\Controller\Admin;
 
 use App\Entity\Voucher;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TelephoneField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 
 class VoucherCrudController extends AbstractCrudController
 {
@@ -12,14 +22,32 @@ class VoucherCrudController extends AbstractCrudController
         return Voucher::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
+        $types = [
+            '代理商-请货' => 0,
+            '门店-请货' => 1,
+            '总公司-代理商退货' => 2,
+            '代理商-门店退货' => 3,
+            '总公司-代理商提现' => 4,
+            '代理商-门店提现' => 5,
+            '餐厅-顾客消费' => 6,
+            '代理商-出货' => 10,
+            '门店-出货' => 11,
+            '代理商退货' => 12,
+            '门店退货' => 13,
+            '代理商-提现' => 14,
+            '门店-提现' => 15,
+            '顾客-餐厅消费' => 16,
+            '内部调控' => 30,
+        ];
+
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->onlyOnIndex(),
+            AssociationField::new('org'),
+            MoneyField::new('voucher')->setCurrency('CNY'),
+            ChoiceField::new('type')->setChoices($types),
+            DateTimeField::new('date')->HideOnForm(),
         ];
     }
-    */
 }
