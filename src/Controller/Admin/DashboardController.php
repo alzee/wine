8f2cd98da79f7506a9745e1088665d7dc6501e66
Al Zee <z@alz.ee>
2022-09-06 17:59:47 +0800

    nth

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -58,23 +58,23 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-chart-simple');
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
-        yield MenuItem::linkToCrud('Product', 'fas fa-list', Product::class);
+        yield MenuItem::linkToCrud('Product', 'fas fa-wine-bottle', Product::class);
         //yield MenuItem::linkToCrud('ProductAgency', 'fas fa-list', ProductAgency::class);
         //yield MenuItem::linkToCrud('ProductStore', 'fas fa-list', ProductStore::class);
         //yield MenuItem::linkToCrud('ProductRestaurant', 'fas fa-list', ProductRestaurant::class);
-        yield MenuItem::linkToCrud('Orders', 'fas fa-list', Orders::class);
+        yield MenuItem::linkToCrud('Orders', 'fas fa-receipt', Orders::class);
         //yield MenuItem::linkToCrud('OrderAgency', 'fas fa-list', OrderAgency::class);
         //yield MenuItem::linkToCrud('OrderStore', 'fas fa-list', OrderStore::class);
         //yield MenuItem::linkToCrud('OrderRestaurant', 'fas fa-list', OrderRestaurant::class);
         //yield MenuItem::linkToCrud('Agency', 'fas fa-list', Agency::class);
         //yield MenuItem::linkToCrud('Store', 'fas fa-list', Store::class);
-        yield MenuItem::linkToCrud('Org', 'fas fa-list', Org::class);
-        yield MenuItem::linkToCrud('Restaurant', 'fas fa-list', Restaurant::class);
-        yield MenuItem::linkToCrud('Voucher', 'fas fa-list', Voucher::class);
-        yield MenuItem::linkToCrud('Returns', 'fas fa-list', Returns::class);
-        yield MenuItem::linkToCrud('Consumer', 'fas fa-list', Consumer::class);
-        yield MenuItem::linkToCrud('User', 'fas fa-list', User::class);
+        yield MenuItem::linkToCrud('Org', 'fas fa-building', Org::class);
+        yield MenuItem::linkToCrud('Restaurant', 'fas fa-utensils', Restaurant::class);
+        yield MenuItem::linkToCrud('Voucher', 'fas fa-money-bill', Voucher::class);
+        yield MenuItem::linkToCrud('Returns', 'fas fa-cart-arrow-down', Returns::class);
+        yield MenuItem::linkToCrud('Consumer', 'fas fa-users', Consumer::class);
+        yield MenuItem::linkToCrud('User', 'fas fa-user', User::class);
     }
 }
