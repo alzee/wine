d5ab3a81f7105a9e9ee0be246b352e3f0215fc1a
Al Zee <z@alz.ee>
2022-09-07 22:47:11 +0800

    nth

--- src/Controller/Admin/ConsumerCrudController.php
+++ src/Controller/Admin/ConsumerCrudController.php
@@ -4,6 +4,16 @@ namespace App\Controller\Admin;
 
 use App\Entity\Consumer;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TelephoneField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 
 class ConsumerCrudController extends AbstractCrudController
 {
@@ -12,14 +22,12 @@ class ConsumerCrudController extends AbstractCrudController
         return Consumer::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->onlyOnIndex(),
+            TextField::new('openid'),
+            MoneyField::new('voucher')->setCurrency('CNY'),
         ];
     }
-    */
 }
