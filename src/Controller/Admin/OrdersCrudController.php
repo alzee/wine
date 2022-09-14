dddf5a6b4fd76cd020d6c25c45841a4a9a0f271b
Al Zee <z@alz.ee>
2022-09-14 08:51:33 +0800

    buildin findOneBy() is fine

--- src/Controller/Admin/OrdersCrudController.php
+++ src/Controller/Admin/OrdersCrudController.php
@@ -35,39 +35,25 @@ class OrdersCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
-        $prod = $this->doctrine->getRepository(Product::class)->find(1);
-        $org = $this->doctrine->getRepository(Org::class)->find($prod->getOrg());
+        $orgRepo = $this->doctrine->getRepository(Org::class);
         $user = $this->getUser();
-        $userOrg = $this->doctrine->getRepository(Org::class)->find($user->getOrg());
-        dump($prod);
-        dump($org);
-        dump($userOrg);
+        $userOrg = $orgRepo->findOneBy(['id' => $user->getOrg()]);
         $roles = $user->getRoles();
         if ($this->isGranted('ROLE_HEAD')) {
-            $upStreamOrgType = 0;
-            $downStreamOrgType = 1;
-            $seller = $this->doctrine->getRepository(Org::class)->findOneByType(0);
+            $seller = $orgRepo->findOneByType(0);
         }
         if ($this->isGranted('ROLE_AGENCY')) {
-            $upStreamOrgType = 0;
-            $downStreamOrgType = 1;
-            $seller = $this->doctrine->getRepository(Org::class)->findOneByType(0);
+            $seller = $orgRepo->findOneByType(0);
         }
         if ($this->isGranted('ROLE_STORE')) {
-            $upStreamOrgType = 1;
-            $downStreamOrgType = 2;
-            $userOrg = $this->doctrine->getRepository(Org::class)->find($user->getOrg());
-            // $seller = $this->doctrine->getRepository(Org::class)->find($userOrg->getUpstream());
-            $seller = $this->doctrine->getRepository(Org::class)->find($userOrg);
+            $seller = $orgRepo->find($userOrg->getUpstream());
         }
         return [
             IdField::new('id')->onlyOnIndex(),
             AssociationField::new('seller')->setQueryBuilder(
-                // fn (QueryBuilder $qb) => $qb->andWhere('entity.type = :type')->setParameter('type', $upStreamOrgType)
                 fn (QueryBuilder $qb) => $qb->andWhere('entity.id = :id')->setParameter('id', $seller)
             ),
             AssociationField::new('buyer')->setQueryBuilder(
-                // fn (QueryBuilder $qb) => $qb->andWhere('entity.type = :type')->setParameter('type', $downStreamOrgType)
                 fn (QueryBuilder $qb) => $qb->andWhere('entity.id = :id')->setParameter('id', $user->getOrg())
             ),
             AssociationField::new('product')->setQueryBuilder(
