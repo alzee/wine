9fd68c016dc6ed752711371791f42ff6cf1a9cdf
Al Zee <z@alz.ee>
2022-09-14 19:55:33 +0800

    PasswordType
    https://github.com/EasyCorp/EasyAdminBundle/issues/3349#issuecomment-695214741

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -11,6 +11,8 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -25,7 +27,15 @@ class UserCrudController extends AbstractCrudController
             IdField::new('id')->onlyOnIndex(),
             AssociationField::new('org'),
             TextField::new('username'),
-            TextField::new('password')->onlyOnForms(),
+            TextField::new('password')->onlyOnForms()
+                                      ->setFormTypeOptions([
+                                          'type' => PasswordType::class,
+                                          'first_options' => ['label' => 'Password'],
+                                          'second_options' => ['label' => 'Repeat password'],
+                                      ])
+                                      ->setFormType(RepeatedType::class)
+                                      ,
+
         ];
     }
 }
