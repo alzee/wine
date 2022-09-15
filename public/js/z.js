7405dd37a7df17de880a5185be22a42cc46e05d7
Al Zee <z@alz.ee>
2022-09-15 17:03:21 +0800

    now in org form restaurant can choose upstream

--- public/js/z.js
+++ public/js/z.js
@@ -3,7 +3,7 @@ console.log('hi, z');
 setTimeout(() => {
     let item = document.querySelector('#Org_type-ts-control .item');
     let upstream = document.querySelector('.upstream');
-    if (item && item.dataset.value == '2') {
+    if (item && item.dataset.value > '1') {
         upstream.classList.remove('d-none');
     }
 
@@ -11,7 +11,7 @@ setTimeout(() => {
     const config = { attributes: true, childList: true, subtree: true };
     const callback = function (){
         item = document.querySelector('#Org_type-ts-control .item');
-        if (item && item.dataset.value == '2') {
+        if (item && item.dataset.value > '1') {
             upstream.classList.remove('d-none');
         } else {
             upstream.classList.add('d-none');
