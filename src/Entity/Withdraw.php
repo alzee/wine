0085c03cbf8c66b27412ca34ad166b92cd81e77b
Al Zee <z@alz.ee>
2022-09-18 11:10:38 +0800

    amount positive validator in withdraw

--- src/Entity/Withdraw.php
+++ src/Entity/Withdraw.php
@@ -6,6 +6,7 @@ use App\Repository\WithdrawRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: WithdrawRepository::class)]
 #[ApiResource]
@@ -20,6 +21,7 @@ class Withdraw
     private ?\DateTimeInterface $date = null;
 
     #[ORM\Column(options: ["unsigned" => true])]
+    #[Assert\Positive]
     private ?int $amount = null;
 
     #[ORM\Column(type: Types::SMALLINT)]
