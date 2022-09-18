226c281a1a83e47f39f4a84df6aee07bca989a97
Al Zee <z@alz.ee>
2022-09-18 10:43:29 +0800

    openid/phone uniqueEntity validator

--- src/Entity/Consumer.php
+++ src/Entity/Consumer.php
@@ -7,8 +7,18 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: ConsumerRepository::class)]
+#[UniqueEntity(
+    fields: 'openid',
+    message: 'Openid is already in use',
+)]
+#[UniqueEntity(
+    fields: 'phone',
+    message: 'Phone is already in use',
+)]
 #[ApiResource]
 class Consumer
 {
