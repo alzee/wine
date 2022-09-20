bb7612d3b63ef7bdc84d86e8db1bb3c74f53231f
Al Zee <z@alz.ee>
2022-09-20 17:35:26 +0800

    nth

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -113,15 +113,15 @@ class DashboardController extends AbstractDashboardController
         if (!$this->isGranted('ROLE_RESTAURANT')) {
             yield MenuItem::linkToCrud('Orders', 'fas fa-receipt', Orders::class);
         }
+        if (!$this->isGranted('ROLE_RESTAURANT')) {
+            yield MenuItem::linkToCrud('Returns', 'fas fa-cart-arrow-down', Returns::class);
+        }
         if ($this->isGranted('ROLE_STORE') || $this->isGranted('ROLE_RESTAURANT')) {
             yield MenuItem::linkToCrud('Retail', 'fas fa-bag-shopping', Retail::class);
             yield MenuItem::linkToCrud('RetailReturn', 'fas fa-cart-shopping', RetailReturn::class);
         }
         if ($this->isGranted('ROLE_RESTAURANT')) {
             yield MenuItem::linkToCrud('OrderRestaurant', 'fas fa-utensils', OrderRestaurant::class);
-        }
-        if (!$this->isGranted('ROLE_RESTAURANT')) {
-            yield MenuItem::linkToCrud('Returns', 'fas fa-cart-arrow-down', Returns::class);
         }
         if (!$this->isGranted('ROLE_STORE')) {
             yield MenuItem::linkToCrud('Withdraw', 'fas fa-money-bill', Withdraw::class);
