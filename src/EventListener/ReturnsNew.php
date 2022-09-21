99c0cc69665993c793e637048c0abca399292668
Al Zee <z@alz.ee>
2022-09-21 10:47:23 +0800

    return event move to postPersist

--- src/EventListener/ReturnsNew.php
+++ src/EventListener/ReturnsNew.php
@@ -11,6 +11,9 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\Returns;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
+use App\Entity\Product;
+use App\Entity\Voucher;
+use App\Entity\Choice;
 
 class ReturnsNew extends AbstractController
 {
@@ -32,9 +35,50 @@ class ReturnsNew extends AbstractController
 
         $return->setAmount($amount);
         $return->setVoucher($voucher);
+        $return->setStatus(5);
     }
 
-    public function postPersist(User $user, LifecycleEventArgs $event): void
+    public function postPersist(Returns  $return, LifecycleEventArgs $event): void
     {
+        $em = $event->getEntityManager();
+
+        $sender = $return->getSender();
+        $recipient = $return->getRecipient();
+        foreach ($return->getReturnItems() as $i) {
+            $product = $i->getProduct();
+            $quantity = $i->getQuantity();
+            $sn = $product->getSn();
+            $price = $product->getPrice();
+            $unitVoucher = $product->getVoucher();
+            // recipient product stock + quantity
+            $product->setStock($product->getStock() + $quantity);
+            // sender product stock - quantity
+            $sender_product = $em->getRepository(Product::class)->findOneByOrgAndSN($sender, $sn);
+            $sender_product->setStock($sender_product->getStock() - $quantity);
+        }
+
+        $voucher = $return->getVoucher();
+        // sender voucher - voucher
+        $sender->setVoucher($sender->getVoucher() - $voucher);
+
+        // recipient voucher + voucher
+        $recipient->setVoucher($recipient->getVoucher() + $voucher);
+
+        // voucher record for sender
+        $record = new Voucher();
+        $record->setOrg($sender);
+        $record->setVoucher(-$voucher);
+        $type = Choice::VOUCHER_TYPES['退货发出'];
+        $record->setType($type);
+        $em->persist($record);
+
+        // voucher record for recipient 
+        $record = new Voucher();
+        $record->setOrg($recipient);
+        $record->setVoucher($voucher);
+        $record->setType($type - 100);
+        $em->persist($record);
+
+        $em->flush();
     }
 }
