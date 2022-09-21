ab5c0a7f209ac52d578e5a90b087dd6f3d32fdcf
Al Zee <z@alz.ee>
2022-09-21 10:39:04 +0800

    order event move to postPersist

--- src/EventListener/OrdersNew.php
+++ src/EventListener/OrdersNew.php
@@ -11,6 +11,9 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\Orders;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
+use App\Entity\Product;
+use App\Entity\Voucher;
+use App\Entity\Choice;
 
 class OrdersNew extends AbstractController
 {
@@ -34,7 +37,54 @@ class OrdersNew extends AbstractController
         $order->setVoucher($voucher);
     }
 
-    public function postPersist(User $user, LifecycleEventArgs $event): void
+    public function postPersist(Orders $order, LifecycleEventArgs $event): void
     {
+        $em = $event->getEntityManager();
+
+        $seller = $order->getSeller();
+        $buyer = $order->getBuyer();
+        foreach ($order->getOrderItems() as $i) {
+            $product = $i->getProduct();
+            $sn = $product->getSn();
+            $quantity = $i->getQuantity();
+            $price = $product->getPrice();
+            $unitVoucher = $product->getVoucher();
+            // product stock - quantity
+            $product->setStock($product->getStock() - $quantity);
+            // if not find this product in buyer org, create it
+            $buyer_product = $em->getRepository(Product::class)->findOneByOrgAndSN($buyer, $sn);
+            if (is_null($buyer_product)) {
+                $buyer_product = clone $product;
+                $buyer_product->setStock(0);
+                $buyer_product->setOrg($buyer);
+                $em->persist($buyer_product);
+            }
+            // buyer stock + quantity
+            $buyer_product->setStock($buyer_product->getStock() + $quantity);
+        }
+
+        $voucher = $order->getVoucher();
+        // seller voucher - voucher
+        $seller->setVoucher($seller->getVoucher() - $voucher);
+
+        // buyer voucher + voucher
+        $buyer->setVoucher($buyer->getVoucher() + $voucher);
+
+        // voucher record for seller
+        $record = new Voucher();
+        $record->setOrg($seller);
+        $record->setVoucher(-$voucher);
+        $type = Choice::VOUCHER_TYPES['发货'];
+        $record->setType($type);
+        $em->persist($record);
+
+        // voucher record for buyer 
+        $record = new Voucher();
+        $record->setOrg($buyer);
+        $record->setVoucher($voucher);
+        $record->setType($type - 100);
+        $em->persist($record);
+
+        $em->flush();
     }
 }
--- src/EventListener/OrdersUpdate.php
+++ src/EventListener/OrdersUpdate.php
@@ -73,6 +73,5 @@ class OrdersUpdate
                 $em->flush();
             }
         }
-
     }
 }
