67a7a4766f5d8ffda336a54cb9310ac8a31b1484
Al Zee <z@alz.ee>
2022-09-26 14:24:21 +0800

    consumer login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,12 +3,25 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use App\Entity\Consumer;
+use Doctrine\Persistence\ManagerRegistry;
 
 class SecurityController extends AbstractController
 {
+    private $doctrine;
+    private $client;
+
+    public function __construct(ManagerRegistry $doctrine, HttpClientInterface $client)
+    {
+        $this->httpclient = $client;
+        $this->doctrine = $doctrine;
+    }
+
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -59,7 +72,34 @@ class SecurityController extends AbstractController
     }
 
     #[Route(path: '/api/consumer_login', name: 'api_consumer_login', methods: ['POST'])]
-    public function consumerLogin()
+    public function consumerLogin(Request $request)
     {
+        $data = json_decode($request->getContent());
+        $code = $data->code;
+        $appid = $_ENV['WX_APP_ID'];
+        $secret = $_ENV['WX_APP_SECRET'];
+        $url = "https://api.weixin.qq.com/sns/jscode2session?appid=$appid&secret=$secret&js_code=$code&grant_type=authorization_code";
+        $header[] = 'Content-Type: application/json';
+        $header[] = 'Accept:application/json';
+        $content = $this->httpclient->request('GET', $url ,['headers' => $header])->toArray();
+        $sessionKey = $content['session_key'];
+        $openid = $content['openid'];
+
+        $consumer = $this->doctrine->getRepository(Consumer::class)->findOneBy(['openid' => $openid]);
+        if (is_null($consumer)) {
+            // create
+            $em = $this->doctrine->getManager();
+            $consumer = new Consumer();
+            $consumer->setOpenid($openid);
+            $em->persist($consumer);
+            $em->flush();
+        }
+
+        $resp = [
+            // "uid" => $consumer->getId(),
+            "role" => 4,
+            "voucher" => $consumer->getVoucher()
+        ];
+        return $this->json($resp);
     }
 }
