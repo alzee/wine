7a6bef6e291ad73186b678a687975d101fae7b5a
Al Zee <z@alz.ee>
2022-09-26 11:31:46 +0800

    nth

--- src/Entity/Org.php
+++ src/Entity/Org.php
@@ -7,15 +7,13 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
-use ApiPlatform\Doctrine\Orm\Filter\NumericFilter;
 
 #[ORM\Entity(repositoryClass: OrgRepository::class)]
 #[ApiResource]
 #[ApiFilter(SearchFilter::class, properties: ['id' => 'exact', 'type' => 'exact'])]
-#[ApiFilter(NumericFilter::class, properties: ['type'])]
 class Org
 {
     #[ORM\Id]
