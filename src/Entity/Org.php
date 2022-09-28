226b0314e83bf66e6912887f5636159578fdcc55
Al Zee <z@alz.ee>
2022-09-28 16:02:52 +0800

    longitude/latitude in entity org

--- src/Entity/Org.php
+++ src/Entity/Org.php
@@ -90,6 +90,12 @@ class Org
     #[Groups(['read'])]
     private ?int $withdrawable = 0;
 
+    #[ORM\Column(nullable: true)]
+    private ?float $longitude = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $latitude = null;
+
     public function __construct()
     {
         $this->orders = new ArrayCollection();
@@ -469,6 +475,30 @@ class Org
     public function setWithdrawable(int $withdrawable): self
     {
         $this->withdrawable = $withdrawable;
+
+        return $this;
+    }
+
+    public function getLongitude(): ?float
+    {
+        return $this->longitude;
+    }
+
+    public function setLongitude(?float $longitude): self
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+
+    public function getLatitude(): ?float
+    {
+        return $this->latitude;
+    }
+
+    public function setLatitude(?float $latitude): self
+    {
+        $this->latitude = $latitude;
 
         return $this;
     }
