fa26ac94bb429e5a21a6fd2d9c065ee3aeede8e1
Al Zee <z@alz.ee>
2022-10-07 23:43:03 +0800

    nth

--- src/EventSubscriber/ExceptionSubscriber.php
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -6,25 +6,36 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Doctrine\DBAL\Exception\DriverException;
 
 class ExceptionSubscriber implements EventSubscriberInterface
 {
     public function onKernelException(ExceptionEvent $event): void
     {
         // ...
+        // dd($event);
         $exception = $event->getThrowable();
-        $message = sprintf(
-            '代金券不足！错误代码: %s',
-            // $exception->getMessage(),
-            $exception->getCode()
-        );
+        dump($exception);
 
         // Customize your response object to display the exception details
-        $response = new Response();
-        $response->setContent($message);
+        if ($exception instanceof NotFoundHttpException) {
+        }
+        if ($exception instanceof AccessDeniedException) {
+        }
+        if ($exception instanceof DriverException){
+            $message = sprintf(
+                '代金券不足！错误代码: %s',
+                // $exception->getMessage(),
+                $exception->getCode()
+            );
+            // sends the modified response object to the event
+            $response = new Response();
+            $response->setContent($message);
 
-        // sends the modified response object to the event
-        $event->setResponse($response);
+            $event->setResponse($response);
+        }
     }
 
     public static function getSubscribedEvents(): array
