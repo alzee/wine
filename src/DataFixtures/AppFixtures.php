43fbdc33dcabb1c186874045beb4cd7dc4555359
Al Zee <z@alz.ee>
2022-10-11 16:50:49 +0800

    fixtures work

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,7 +11,17 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         // $product = new Product();
-        // $manager->persist($product);
+        // create 20 products! Bam!
+        for ($i = 0; $i < 20; $i++) {
+            $product = new Product();
+            $product->setName('product '.$i);
+            $product->setSpec('spec'.$i);
+            $product->setSn('sn'.$i);
+            $product->setVoucher(500);
+            $product->setPrice(mt_rand(10, 100));
+            $manager->persist($product);
+        }
+        $manager->persist($product);
 
         $manager->flush();
     }
