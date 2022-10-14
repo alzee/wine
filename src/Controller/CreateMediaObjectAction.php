2082aff880c60d6e0ea9186a06fb0b9b980f8ccf
Al Zee <z@alz.ee>
2022-10-15 01:08:42 +0800

    ckeditor file parameter is upload

--- src/Controller/CreateMediaObjectAction.php
+++ src/Controller/CreateMediaObjectAction.php
@@ -13,7 +13,7 @@ final class CreateMediaObjectAction extends AbstractController
 {
     public function __invoke(Request $request): MediaObject
     {
-        $uploadedFile = $request->files->get('file');
+        $uploadedFile = $request->files->get('upload');
         if (!$uploadedFile) {
             throw new BadRequestHttpException('"file" is required');
         }
