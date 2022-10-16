39ebabd8178e8f24255b183e5d55315546cb97f4
Al Zee <z@alz.ee>
2022-10-16 21:35:53 +0800

    nth

--- code.sh
+++ code.sh
@@ -2,5 +2,5 @@
 #
 # vim:ft=sh
 
-find src/ -type f ! -name *.png -exec echo // {} >> code.txt \; -exec cat {} >> code.txt  \; -exec echo >> code.txt \;
+find src/ ~/w/taro.wine/src/ -type f ! -name *.png -exec echo // {} >> code.txt \; -exec cat {} >> code.txt  \; -exec echo >> code.txt \;
 [ -d public/ ] && mv code.txt public/
