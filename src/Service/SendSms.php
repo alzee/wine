83bd8fa4415b9058396d155bae3ca629c3649dd9
Al Zee <z@alz.ee>
2022-12-06 11:56:45 +0800

    service SendSms

---
+++ src/Service/SendSms.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * vim:ft=php et ts=4 sts=4
+ * @author Al Zee <z@alz.ee>
+ * @version
+ * @todo
+ */
+
+namespace App\Service;
+
+
+class SendSms
+{
+    private $a;
+    private $b;
+    private $c;
+    
+    public function __construct()
+    {
+    }
+
+    public function __destruct()
+    {
+    }
+}
+
