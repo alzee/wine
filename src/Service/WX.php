8aec4039e533b7b96b4d197cf087f8629301a450
Al Zee <z@alz.ee>
2022-12-29 18:41:19 +0800

    service WX

---
+++ src/Service/WX.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * vim:ft=php et ts=4 sts=4
+ * @author Al Zee <z@alz.ee>
+ * @version
+ * @todo
+ */
+
+namespace App\Service;
+
+use Symfony\Component\Cache\Adapter\RedisAdapter;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class WX
+{
+    private $httpClient;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->httpClient = $client;
+    }
+
+    public function getAccessToken()
+    {
+        $cache = new RedisAdapter(RedisAdapter::createConnection('redis://localhost'));
+
+        $cache->get('WX_ACCESS_TOKEN', function (ItemInterface $item) {
+            $item->expiresAfter(7200);
+            $appid = $_ENV['WX_APP_ID'];
+            $secret = $_ENV['WX_APP_SECRET'];
+            $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appid}&secret=${secret}";
+            $content = $this->httpClient->request('GET', $url)->toArray();
+            dump($content);
+            return $content['access_token'];
+        });
+
+    }
+}
