ccdc43dee593dcb49a5f5545ce2e0e3f90f2d6e5
Al Zee <z@alz.ee>
2023-01-10 16:20:06 +0800

    resize_images.sh

---
+++ resize_images.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# vim:ft=sh
+
+############### Variables ###############
+
+############### Functions ###############
+
+############### Main Part ###############
+
+pushd public/img/
+
+for dir in {node, org, poster, product, withdraw}
+do
+    pushd dir
+    mogrify -format jpg -path thumbnail -thumbnail 200 -quality 69 *.{jpg,png}
+    for i in *.{jpg,png}
+    do
+        convert $i -format jpg -resize 400 -quality 75 $i
+    done
+    popd
+done
+
+popd
