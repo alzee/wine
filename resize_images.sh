9a9e1331e470c98531a453aed1a72a869295a575
Al Zee <z@alz.ee>
2023-01-10 16:45:36 +0800

    nth

--- resize_images.sh
+++ resize_images.sh
@@ -14,7 +14,7 @@ for dir in {node,org,poster,product,withdraw}
 do
     pushd $dir
     mogrify -format jpg -path thumbnail -thumbnail 200 -quality 67 *.{jpg,png}
-    for i in *.{jpg,png}
+    for i in *.jpg
     do
         convert $i -format jpg -resize 400 -quality 75 $i
         # identify $i
