f25c1dd33c9f813f8d9c22dda641eecc4dc8ebdb
Al Zee <z@alz.ee>
2023-01-10 13:53:44 +0800

    upload event

---
+++ src/EventListener/Upload.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * vim:ft=php et ts=4 sts=4
+ * @version
+ * @todo
+ */
+
+namespace App\EventListener;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Entity\Org;
+use Vich\UploaderBundle\Event\Event;
+
+class Upload extends AbstractController
+{
+    public function onVichUploaderPostUpload(Event $event): void
+    {
+        $object = $event->getObject();
+        $mapping = $event->getMapping();
+        dump($object);
+        dump($mapping);
+    }
+}
