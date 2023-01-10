7350b25552d87400ae9d97948fbbe7a9ea001856
Al Zee <z@alz.ee>
2023-01-10 15:15:29 +0800

    set_permission.sh

---
+++ set_permission.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# vim:ft=sh
+
+############### Variables ###############
+
+############### Functions ###############
+
+############### Main Part ###############
+find public/img/ -type d -exec chmod 775 {} \;
