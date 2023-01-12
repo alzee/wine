b52c8189e64a00ba4ff581056f0d591dfb9102ee
Al Zee <z@alz.ee>
2023-01-12 13:33:36 +0800

    add stock to head creating product

--- src/EventListener/ProductNew.php
+++ src/EventListener/ProductNew.php
@@ -10,6 +10,7 @@ namespace App\EventListener;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\Product;
 use App\Entity\Org;
+use App\Entity\Stock;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
 
 class ProductNew extends AbstractController
@@ -24,7 +25,15 @@ class ProductNew extends AbstractController
         }
     }
 
-    public function postPersist(User $user, LifecycleEventArgs $event): void
+    public function postPersist(Product $product, LifecycleEventArgs $event): void
     {
+        $em = $event->getEntityManager();
+        $head = $em->getRepository(Org::class)->findOneBy(['type' => 0]);
+        $stockRecord = new Stock;
+        $stockRecord->setStock(0);
+        $stockRecord->setOrg($head);
+        $stockRecord->setProduct($product);
+        $em->persist($stockRecord);
+        $em->flush();
     }
 }
