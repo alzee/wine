5b7272f6fac78474f1c02ab67b9fc30afe6c6452
Al Zee <z@alz.ee>
2023-01-13 04:05:30 +0800

    so must explicitly use BooleanField to enable AJAX update

--- src/Controller/Admin/IndustryCrudController.php
+++ src/Controller/Admin/IndustryCrudController.php
@@ -5,6 +5,10 @@ namespace App\Controller\Admin;
 use App\Entity\Industry;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 
 class IndustryCrudController extends AbstractCrudController
 {
@@ -13,16 +17,13 @@ class IndustryCrudController extends AbstractCrudController
         return Industry::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield IdField::new('id');
+        yield TextField::new('name');
+        yield IntegerField::new('weight');
+        yield BooleanField::new('isOff');
     }
-    */
 
     public function configureCrud(Crud $crud): Crud
     {
