1e43138705a0c2c7a156d1800fc31a3874b1da2b
Al Zee <z@alz.ee>
2023-01-12 12:14:21 +0800

    stock crud fields

--- src/Controller/Admin/StockCrudController.php
+++ src/Controller/Admin/StockCrudController.php
@@ -4,6 +4,19 @@ namespace App\Controller\Admin;
 
 use App\Entity\Stock;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
+use Doctrine\ORM\QueryBuilder;
+use EasyCorp\Bundle\EasyAdminBundle\Orm\EntityRepository;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 
 class StockCrudController extends AbstractCrudController
 {
@@ -12,14 +25,54 @@ class StockCrudController extends AbstractCrudController
         return Stock::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            // AssociationField::new('org'),
+            // AssociationField::new('product'),
+            TextField::new('product.sn', 'Sn'),
+            TextField::new('product.name', 'Name'),
+            TextField::new('product.spec', 'Spec'),
+            MoneyField::new('product.price', 'Price')->setCurrency('CNY'),
+            IntegerField::new('stock'),
+            MoneyField::new('product.voucher', 'Voucher')
+                ->setCurrency('CNY')
+                ->setHelp('<b>代金券</b>为本件商品随增的代金券金额')
+            ,
+            MoneyField::new('product.refReward', 'Ref Reward')
+                ->setCurrency('CNY')
+            ,
+            MoneyField::new('product.orgRefReward', 'Org Ref Reward')
+                ->setCurrency('CNY')
+            ,
+            MoneyField::new('product.partnerReward', 'Partner Reward')
+                ->setCurrency('CNY')
+            ,
+            MoneyField::new('product.offIndustryStoreReward', 'Off Industry Store Reward')
+                ->setCurrency('CNY')
+            ,
+            MoneyField::new('product.offIndustryAgencyReward', 'Off Industry Agency Reward')
+                ->setCurrency('CNY')
+            ,
+            ImageField::new('product.img', 'Product Image')
+                ->onlyOnIndex()
+                ->setBasePath('img/product/thumbnail/')
+            ,
         ];
     }
-    */
+
+    public function createIndexQueryBuilder(SearchDto $searchDto, EntityDto $entityDto, FieldCollection $fields, FilterCollection $filters): QueryBuilder
+    {
+        $userOrg = $this->getUser()->getOrg()->getId();
+        $response = $this->container->get(EntityRepository::class)->createQueryBuilder($searchDto, $entityDto, $fields, $filters);
+        $response->andWhere("entity.org = $userOrg");
+        return $response;
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->disable(Action::DELETE, Action::NEW, Action::EDIT)
+        ;
+    }
 }
