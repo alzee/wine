599e44024ab779f885d33a3f19e2b9c682d6e180
Al Zee <z@alz.ee>
2023-01-14 13:54:17 +0800

    nth

--- resize_images.sh
+++ resize_images.sh
@@ -13,9 +13,8 @@ pushd public/img/
 for dir in {node,org,product,withdraw}
 do
     pushd $dir
-    mogrify -format jpg -path thumbnail -thumbnail 100 -quality 60 *.{jpg,png}
-    mogrify -format jpg -resize 400 -quality 75 *.{jpg,png}
-    rm *.png
+    mogrify -format jpg -resize 400 -quality 75 *.{jpg,png} && rm *.png
+    mogrify -format jpg -path thumbnail -thumbnail 100 -quality 60 *.jpg
     popd
 done
 
