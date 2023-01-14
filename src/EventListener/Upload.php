3e46d8895fa2acd43e3bac6596359f0838705397
Al Zee <z@alz.ee>
2023-01-14 13:40:25 +0800

    crop image in event upload

--- src/EventListener/Upload.php
+++ src/EventListener/Upload.php
@@ -13,19 +13,33 @@ class Upload
 {
     public function onVichUploaderPostUpload(Event $event): void
     {
+        $width = 400;
+        $height = 200;
+        $thumbnail_width = 100;
         $object = $event->getObject();
         // $mapping = $event->getMapping();
         $file = $object->getImageFile();
         $file_path = $file->getPathname();
         $info = getimagesize($file_path);
         $new_file = match ($info['mime']) {
-        'image/jpeg' => imagecreatefromjpeg($file_path),
-        'image/png' => imagecreatefrompng($file_path),
-        'image/gif' => imagecreatefromgif($file_path),
+        'image/jpeg' => imagescale(imagecreatefromjpeg($file_path), $width),
+        'image/png' => imagescale(imagecreatefrompng($file_path), $width),
         };
 
-        imagejpeg(imagescale($new_file, 100), $file->getPath() . '/thumbnail/' . preg_replace('/.png/i', '.jpg', $file->getFilename()), 60);
-        imagejpeg(imagescale($new_file, 400), preg_replace('/.png/i', '.jpg', $file_path), 75);
+        $new_height = $width / $info[0] * $info[1];
+        if ($new_height > $height) {
+            $offset_y = ($new_height - $height) / 2;
+        } else {
+            $offset_y = 0;
+        }
+        // crop height
+        $new_file = imagecrop($new_file, ['x' => 0, 'y' => $offset_y, 'width' => $width, 'height' => $height]);
+
+        // save thumbnail
+        imagejpeg(imagescale($new_file, $thumbnail_width), $file->getPath() . '/thumbnail/' . preg_replace('/.png/i', '.jpg', $file->getFilename()), 60);
+
+        // save image
+        imagejpeg($new_file, preg_replace('/.png/i', '.jpg', $file_path), 75);
 
         if ($info['mime'] != 'image/jpeg') {
             unlink($file_path);
