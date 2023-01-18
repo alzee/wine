ecf2292a13a62df28b187f4e73e12d8e621e7b5a
Al Zee <z@alz.ee>
2023-01-19 04:33:12 +0800

    reward entity

---
+++ src/Entity/Reward.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RewardRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+
+#[ORM\Entity(repositoryClass: RewardRepository::class)]
+#[ApiResource(
+    normalizationContext: ['groups' => ['read']],
+    denormalizationContext: ['groups' => ['write']],
+)]
+#[ApiFilter(SearchFilter::class, properties: ['consumer' => 'exact', 'org' => 'exact', 'retail' => 'exact', 'ord' => 'exact', 'type' => 'exact'])]
+class Reward
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    #[Groups(['read'])]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    #[Groups(['read'])]
+    private ?int $type = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    #[Groups(['read'])]
+    private ?int $amount = null;
+
+    #[ORM\Column]
+    #[Groups(['read'])]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\ManyToOne]
+    #[Groups(['read'])]
+    private ?Consumer $consumer = null;
+
+    #[ORM\ManyToOne]
+    #[Groups(['read'])]
+    private ?Retail $retail = null;
+
+    #[ORM\ManyToOne]
+    #[Groups(['read'])]
+    private ?Org $org = null;
+
+    #[ORM\ManyToOne]
+    #[Groups(['read'])]
+    private ?Orders $ord = null;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getType(): ?int
+    {
+        return $this->type;
+    }
+
+    public function setType(int $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getAmount(): ?int
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(int $amount): self
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getConsumer(): ?Consumer
+    {
+        return $this->consumer;
+    }
+
+    public function setConsumer(?Consumer $consumer): self
+    {
+        $this->consumer = $consumer;
+
+        return $this;
+    }
+
+    public function getRetail(): ?Retail
+    {
+        return $this->retail;
+    }
+
+    public function setRetail(?Retail $retail): self
+    {
+        $this->retail = $retail;
+
+        return $this;
+    }
+
+    public function getOrg(): ?Org
+    {
+        return $this->org;
+    }
+
+    public function setOrg(?Org $org): self
+    {
+        $this->org = $org;
+
+        return $this;
+    }
+
+    public function getOrd(): ?Orders
+    {
+        return $this->ord;
+    }
+
+    public function setOrd(?Orders $ord): self
+    {
+        $this->ord = $ord;
+
+        return $this;
+    }
+}
