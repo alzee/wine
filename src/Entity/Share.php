45cf93e1a983279de7321e7fca17dc0c16cb2f10
Al Zee <z@alz.ee>
2023-01-19 04:51:34 +0800

    entity share

---
+++ src/Entity/Share.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ShareRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+
+#[ORM\Entity(repositoryClass: ShareRepository::class)]
+#[ApiResource(
+    normalizationContext: ['groups' => ['read']],
+    denormalizationContext: ['groups' => ['write']],
+)]
+#[ApiFilter(SearchFilter::class, properties: ['org' => 'exact', 'retail' => 'exact', 'type' => 'exact'])]
+class Share
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    #[Groups(['read'])]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    #[Groups(['read'])]
+    private ?int $type = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    #[Groups(['read'])]
+    private ?int $amount = null;
+
+    #[ORM\Column]
+    #[Groups(['read'])]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\ManyToOne]
+    #[Groups(['read'])]
+    private ?Org $org = null;
+
+    #[ORM\ManyToOne]
+    #[Groups(['read'])]
+    private ?Retail $retail = null;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getType(): ?int
+    {
+        return $this->type;
+    }
+
+    public function setType(int $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getAmount(): ?int
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(int $amount): self
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getOrg(): ?Org
+    {
+        return $this->org;
+    }
+
+    public function setOrg(?Org $org): self
+    {
+        $this->org = $org;
+
+        return $this;
+    }
+
+    public function getRetail(): ?Retail
+    {
+        return $this->retail;
+    }
+
+    public function setRetail(?Retail $retail): self
+    {
+        $this->retail = $retail;
+
+        return $this;
+    }
+}
