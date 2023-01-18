7a88b94be44e935081b325842a0e9f53fcc3cb35
Al Zee <z@alz.ee>
2023-01-19 06:09:59 +0800

    nth

--- src/Entity/Org.php
+++ src/Entity/Org.php
@@ -23,7 +23,7 @@ use Symfony\Component\Serializer\Annotation\Ignore;
     denormalizationContext: ['groups' => ['write']],
     paginationEnabled: false,
 )]
-#[ApiFilter(SearchFilter::class, properties: ['id' => 'exact', 'city' => 'exact', 'industry' => 'exact', 'type' => 'exact', 'name' => 'partial'])]
+#[ApiFilter(SearchFilter::class, properties: ['id' => 'exact', 'city' => 'exact', 'industry' => 'exact', 'type' => 'exact', 'name' => 'partial', 'referrer' => 'exact'])]
 #[ApiFilter(BooleanFilter::class, properties: ['display'])]
 #[ApiFilter(OrderFilter::class, properties: ['id'], arguments: ['orderParameterName' => 'order'])]
 class Org
