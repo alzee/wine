dcd4d32aa4d9388bfb4644a83ec2fa22cba7a5f2
Al Zee <z@alz.ee>
2023-01-24 22:28:39 +0800

    rm conf fields

--- src/Entity/Conf.php
+++ src/Entity/Conf.php
@@ -14,72 +14,8 @@ class Conf
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(options: ["unsigned" => true])]
-    #[Assert\Positive]
-    private ?int $refReward = null;
-
-    #[ORM\Column(options: ["unsigned" => true])]
-    #[Assert\Positive]
-    private ?int $partnerReward = null;
-
-    #[ORM\Column(options: ["unsigned" => true])]
-    #[Assert\Positive]
-    private ?int $offIndustryStoreReward = null;
-
-    #[ORM\Column(options: ["unsigned" => true])]
-    #[Assert\Positive]
-    private ?int $offIndustryAgencyReward = null;
-
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getRefReward(): ?int
-    {
-        return $this->refReward;
-    }
-
-    public function setRefReward(int $refReward): self
-    {
-        $this->refReward = $refReward;
-
-        return $this;
-    }
-
-    public function getPartnerReward(): ?int
-    {
-        return $this->partnerReward;
-    }
-
-    public function setPartnerReward(int $partnerReward): self
-    {
-        $this->partnerReward = $partnerReward;
-
-        return $this;
-    }
-
-    public function getOffIndustryStoreReward(): ?int
-    {
-        return $this->offIndustryStoreReward;
-    }
-
-    public function setOffIndustryStoreReward(int $offIndustryStoreReward): self
-    {
-        $this->offIndustryStoreReward = $offIndustryStoreReward;
-
-        return $this;
-    }
-
-    public function getOffIndustryAgencyReward(): ?int
-    {
-        return $this->offIndustryAgencyReward;
-    }
-
-    public function setOffIndustryAgencyReward(int $offIndustryAgencyReward): self
-    {
-        $this->offIndustryAgencyReward = $offIndustryAgencyReward;
-
-        return $this;
     }
 }
