e358af75b26611635759e9c12cb37e26fa0baee1
Al Zee <z@alz.ee>
2023-01-25 01:29:10 +0800

    nth

---
+++ src/EventListener/ShareUpdate.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * vim:ft=php et ts=4 sts=4
+ * @version
+ * @todo
+ */
+
+namespace App\EventListener;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+use App\Entity\Share;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+
+class ShareUpdate
+{
+    public function preUpdate(Share $share, PreUpdateEventArgs $event): void
+    {
+        if ($event->hasChangedField('status')) {
+            $em = $event->getEntityManager();
+            $newStatusId = $event->getNewValue('status')->getId();
+            if ($newStatusId == 1) {
+            }
+        }
+    }
+}
