2308ab24f410320ebdddd3c173b5b382d963550d
Al Zee <z@alz.ee>
2023-01-25 19:24:52 +0800

    nth

--- src/EventListener/WithdrawNew.php
+++ src/EventListener/WithdrawNew.php
@@ -17,18 +17,26 @@ class WithdrawNew extends AbstractController
 
     public function prePersist(Withdraw $withdraw, LifecycleEventArgs $event): void
     {
-        $applicant = $withdraw->getApplicant();
+        $em = $event->getEntityManager();
+        $head = $em->getRepository(Org::class)->findOneBy(['type' => 0]);
         $amount = $withdraw->getAmount();
+        $withdraw->setActualAmount($withdraw->getAmount());
+        $withdraw->setApprover($head);
+        $applicant = $withdraw->getApplicant();
+
+        if (is_null($applicant)) {
+            if ($applicant->getType() == 1 || $applicant->getType() == 3) {
+                $withdraw->setApprover($applicant->getUpstream());
+            }
+
+            if ($applicant->getType() == 3) {
+                $withdraw->setActualAmount($withdraw->getAmount() * $applicant->getDiscount());
+            }
+        } else {
+            $applicant = $withdraw->getConsumer();
+        }
 
         $applicant->setWithdrawable($applicant->getWithdrawable() - $amount);
         $applicant->setWithdrawing($applicant->getWithdrawing() + $amount);
-
-        $withdraw->setApprover($applicant->getUpstream());
-
-        if ($applicant->getType() == 3) {
-            $withdraw->setActualAmount($withdraw->getAmount() * $applicant->getDiscount());
-        } else {
-            $withdraw->setActualAmount($withdraw->getAmount());
-        }
     }
 }
