28eed3866d329517199962f42c27a6fb59ac20ff
Al Zee <z@alz.ee>
2023-02-13 21:17:51 +0800

    remove sn/cipher/bottles/orders in entity Box

--- src/Entity/Box.php
+++ src/Entity/Box.php
@@ -18,18 +18,6 @@ class Box
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $sn = null;
-
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $cipher = null;
-
-    #[ORM\OneToMany(mappedBy: 'box', targetEntity: Bottle::class)]
-    private Collection $bottles;
-
-    #[ORM\OneToMany(mappedBy: 'box', targetEntity: Orders::class)]
-    private Collection $orders;
-
     #[ORM\Column]
     #[Assert\Positive]
     #[Assert\LessThanOrEqual(1000)]
@@ -48,8 +36,6 @@ class Box
 
     public function __construct()
     {
-        $this->bottles = new ArrayCollection();
-        $this->orders = new ArrayCollection();
         $this->boxPrizes = new ArrayCollection();
     }
 
@@ -61,90 +47,6 @@ class Box
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getSn(): ?string
-    {
-        return $this->sn;
-    }
-
-    public function setSn(?string $sn): self
-    {
-        $this->sn = $sn;
-
-        return $this;
-    }
-
-    public function getCipher(): ?string
-    {
-        return $this->cipher;
-    }
-
-    public function setCipher(?string $cipher): self
-    {
-        $this->cipher = $cipher;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Bottle>
-     */
-    public function getBottles(): Collection
-    {
-        return $this->bottles;
-    }
-
-    public function addBottle(Bottle $bottle): self
-    {
-        if (!$this->bottles->contains($bottle)) {
-            $this->bottles->add($bottle);
-            $bottle->setBox($this);
-        }
-
-        return $this;
-    }
-
-    public function removeBottle(Bottle $bottle): self
-    {
-        if ($this->bottles->removeElement($bottle)) {
-            // set the owning side to null (unless already changed)
-            if ($bottle->getBox() === $this) {
-                $bottle->setBox(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Orders>
-     */
-    public function getOrders(): Collection
-    {
-        return $this->orders;
-    }
-
-    public function addOrder(Orders $order): self
-    {
-        if (!$this->orders->contains($order)) {
-            $this->orders->add($order);
-            $order->setBox($this);
-        }
-
-        return $this;
-    }
-
-    public function removeOrder(Orders $order): self
-    {
-        if ($this->orders->removeElement($order)) {
-            // set the owning side to null (unless already changed)
-            if ($order->getBox() === $this) {
-                $order->setBox(null);
-            }
-        }
-
-        return $this;
     }
 
     public function getQuantity(): ?int
