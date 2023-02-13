2781cc28e975786875a50d65c3274115f9bf4414
Al Zee <z@alz.ee>
2023-02-13 09:51:18 +0800

    entity Bottle

---
+++ src/Entity/Bottle.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BottleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BottleRepository::class)]
+class Bottle
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'bottles')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Box $box = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $sn = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBox(): ?Box
+    {
+        return $this->box;
+    }
+
+    public function setBox(?Box $box): self
+    {
+        $this->box = $box;
+
+        return $this;
+    }
+
+    public function getSn(): ?string
+    {
+        return $this->sn;
+    }
+
+    public function setSn(string $sn): self
+    {
+        $this->sn = $sn;
+
+        return $this;
+    }
+}
--- src/Entity/Box.php
+++ src/Entity/Box.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\BoxRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: BoxRepository::class)]
@@ -18,6 +20,14 @@ class Box
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $enc = null;
+
+    #[ORM\OneToMany(mappedBy: 'box', targetEntity: Bottle::class)]
+    private Collection $bottles;
+
+    public function __construct()
+    {
+        $this->bottles = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -44,6 +54,36 @@ class Box
     public function setEnc(?string $enc): self
     {
         $this->enc = $enc;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Bottle>
+     */
+    public function getBottles(): Collection
+    {
+        return $this->bottles;
+    }
+
+    public function addBottle(Bottle $bottle): self
+    {
+        if (!$this->bottles->contains($bottle)) {
+            $this->bottles->add($bottle);
+            $bottle->setBox($this);
+        }
+
+        return $this;
+    }
+
+    public function removeBottle(Bottle $bottle): self
+    {
+        if ($this->bottles->removeElement($bottle)) {
+            // set the owning side to null (unless already changed)
+            if ($bottle->getBox() === $this) {
+                $bottle->setBox(null);
+            }
+        }
 
         return $this;
     }
