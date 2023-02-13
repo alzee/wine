7c1bc8fc73e6cfdfcedcf8d09c2c0fa6f03b7366
Al Zee <z@alz.ee>
2023-02-13 13:12:39 +0800

    service Sn DONE

--- src/Service/Sn.php
+++ src/Service/Sn.php
@@ -13,14 +13,14 @@ class Sn
     public static function gen(int $id)
     {
         // https://stackoverflow.com/a/12001085/7714132
-        $left = 'A00000';
+        $left_start = 'A00000';
         $n = 4;
         $right_range = 10 ** $n;
         $int_quotient = intdiv($id, $right_range);
         if ($int_quotient > 0) {
             $id = $id % $right_range;
         }
-        $left = base_convert(base_convert($left, 36, 10) + $int_quotient, 10, 36);
+        $left = base_convert(base_convert($left_start, 36, 10) + $int_quotient, 10, 36);
         $left = strtoupper($left);
         return $left . str_pad($id, $n, 0, STR_PAD_LEFT);
     }
