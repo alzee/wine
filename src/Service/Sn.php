b0369042a5b9a4edd8c111397f3df10e30403a61
Al Zee <z@alz.ee>
2023-02-13 13:11:59 +0800

    service Sn DONE

---
+++ src/Service/Sn.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * vim:ft=php et ts=4 sts=4
+ * @author Al Zee <z@alz.ee>
+ * @version
+ * @todo
+ */
+
+namespace App\Service;
+
+class Sn
+{
+    public static function gen(int $id)
+    {
+        // https://stackoverflow.com/a/12001085/7714132
+        $left = 'A00000';
+        $n = 4;
+        $right_range = 10 ** $n;
+        $int_quotient = intdiv($id, $right_range);
+        if ($int_quotient > 0) {
+            $id = $id % $right_range;
+        }
+        $left = base_convert(base_convert($left, 36, 10) + $int_quotient, 10, 36);
+        $left = strtoupper($left);
+        return $left . str_pad($id, $n, 0, STR_PAD_LEFT);
+    }
+}
