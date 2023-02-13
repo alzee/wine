4ba750870bd7345764344f81477d78e4149d017f
Al Zee <z@alz.ee>
2023-02-13 15:24:13 +0800

    8 bits is enough

--- src/Service/Sn.php
+++ src/Service/Sn.php
@@ -13,7 +13,10 @@ class Sn
     public static function gen(int $id)
     {
         // https://stackoverflow.com/a/12001085/7714132
-        $left_start = 'A00000';
+        // box sn 8 bits
+        // left 4 bits base36 but started from A000, right 4 bits base10 for readability
+        // 36**8 - 10**8 - 26**4 = 2,821,009,450,480
+        $left_start = 'A000';
         $n = 4;
         $right_range = 10 ** $n;
         $int_quotient = intdiv($id, $right_range);
