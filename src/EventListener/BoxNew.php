41aed625d5743f1cbd60ee5b0396d94a2b5fcb4f
Al Zee <z@alz.ee>
2023-02-17 21:09:55 +0800

    event BoxNew to set default org

---
+++ src/EventListener/BoxNew.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * vim:ft=php et ts=4 sts=4
+ * @version
+ * @todo
+ */
+
+namespace App\EventListener;
+
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Box;
+use App\Entity\Org;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
+use Doctrine\ORM\Events;
+
+#[AsEntityListener(event: Events::prePersist, entity: Box::class)]
+class BoxNew extends AbstractController
+{
+    public function __construct()
+    {
+    }
+
+    public function prePersist(Box $box, LifecycleEventArgs $event): void
+    {
+        $em = $event->getEntityManager();
+        $head = $em->getRepository(Org::class)->findOneBy(['type' => 0]);
+        $box->setOrg($head);
+    }
+}
