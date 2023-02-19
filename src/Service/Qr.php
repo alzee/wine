a92caf43706825fd2acadbd5532efc038b0bf6d1
Al Zee <z@alz.ee>
2023-02-19 10:51:34 +0800

    qr gen type

--- src/Service/Qr.php
+++ src/Service/Qr.php
@@ -27,7 +27,7 @@ class Qr
         $box = $this->em->find('App\Entity\Box', $boxid);
         $boxSn = $box->getSn();
         $boxEnc = explode('.', $box->getCipher())[0];
-        $this->gen($boxSn, $boxEnc);
+        $this->gen($boxSn, $boxEnc, 0);
         $margin_l = 100;
         $margin_t = 100;
         
@@ -39,7 +39,7 @@ class Qr
         foreach ($bottles as $bottle) {
             $sn = $bottle->getSn();
             $enc = explode('.', $bottle->getCipher())[0];
-            $this->gen($sn, $enc);
+            $this->gen($sn, $enc, 1);
             $width = 645;
             $height = 739;
             $ratio = 0.5;
@@ -58,15 +58,15 @@ class Qr
             $offset_y = $offset_y * ceil($bid / $col - 1) + $margin_t;
             
             shell_exec("composite -geometry {$reszied_w}x+{$offset_x}+{$offset_y} {$sn}.png {$boxSn}.png {$boxSn}.png");
-            shell_exec("rm {$sn}.png");
+            // shell_exec("rm {$sn}.png");
         }
     }
     
-    public function gen($sn, $enc) {
-        $url = "https://jiu.itove.com/qr/draw";
-        $text = "{$url}?s={$sn}?e={$enc}";
+    public function gen($sn, $enc, $type) {
+        $url = "https://jiu.itove.com/wxqr";
+        $text = "{$url}?t={$type}&s={$sn}&e={$enc}";
         // -s 15 witth 645px;
-        shell_exec("qrencode -t png -s 15 -m 5 {$text} -o {$sn}.png");
+        shell_exec("qrencode -t png -s 15 -m 5 \"{$text}\" -o {$sn}.png");
         shell_exec("convert {$sn}.png -pointsize 72 label:{$sn}  -gravity Center -append {$sn}.png");
     }
 }
