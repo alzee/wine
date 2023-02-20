8669ad536bef46c203fd241aebdd01d617b1c560
Al Zee <z@alz.ee>
2023-02-20 12:36:31 +0800

    nth

--- src/Entity/Claim.php
+++ src/Entity/Claim.php
@@ -5,6 +5,8 @@ namespace App\Entity;
 use App\Repository\ClaimRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
 use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 
