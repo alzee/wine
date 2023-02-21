2959431768799242ee6b19666b2414d403c49da7
Al Zee <z@alz.ee>
2023-02-21 11:03:58 +0800

    rm fields prize/qty in entity Pack

--- src/Controller/Admin/PackCrudController.php
+++ src/Controller/Admin/PackCrudController.php
@@ -4,6 +4,21 @@ namespace App\Controller\Admin;
 
 use App\Entity\Pack;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DatetimeField;
+use Symfony\Component\HttpFoundation\RequestStack;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 
 class PackCrudController extends AbstractCrudController
 {
@@ -12,14 +27,12 @@ class PackCrudController extends AbstractCrudController
         return Pack::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield IdField::new('id')
+            ->hideOnForm();
+            ;
+        yield TextField::new('name');
+        yield CollectionField::new('packPrize');
     }
-    */
 }
--- src/Entity/Pack.php
+++ src/Entity/Pack.php
@@ -16,59 +16,16 @@ class Pack
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\ManyToMany(targetEntity: Prize::class)]
-    private Collection $prize;
-
-    #[ORM\Column(type: Types::SMALLINT)]
-    private ?int $qty = null;
-
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
     public function __construct()
     {
-        $this->prize = new ArrayCollection();
     }
 
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    /**
-     * @return Collection<int, Prize>
-     */
-    public function getPrize(): Collection
-    {
-        return $this->prize;
-    }
-
-    public function addPrize(Prize $prize): self
-    {
-        if (!$this->prize->contains($prize)) {
-            $this->prize->add($prize);
-        }
-
-        return $this;
-    }
-
-    public function removePrize(Prize $prize): self
-    {
-        $this->prize->removeElement($prize);
-
-        return $this;
-    }
-
-    public function getQty(): ?int
-    {
-        return $this->qty;
-    }
-
-    public function setQty(int $qty): self
-    {
-        $this->qty = $qty;
-
-        return $this;
     }
 
     public function getName(): ?string
