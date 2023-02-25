57facdd88ed6875fff0791c77ebb9d1389722c79
Al Zee <z@alz.ee>
2023-02-25 12:00:09 +0800

    nth

--- src/Entity/Claim.php
+++ src/Entity/Claim.php
@@ -16,7 +16,7 @@ use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
     denormalizationContext: ['groups' => ['write']],
     paginationEnabled: false,
 )]
-#[ApiFilter(SearchFilter::class, properties: ['store' => 'exact', 'customer' => 'exact', 'status' => 'exact'])]
+#[ApiFilter(SearchFilter::class, properties: ['id' => 'exact', 'store' => 'exact', 'customer' => 'exact', 'status' => 'exact'])]
 class Claim
 {
     #[ORM\Id]
