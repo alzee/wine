082156fec159c32af21327e21a801fff8b080124
Al Zee <z@alz.ee>
2023-02-28 09:41:07 +0800

    rm APIs: order/new, return/new, retail/new, retail_return/new, chpwd, resetpwd, sms, chktop, chkphone, pca

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -47,40 +47,6 @@ class ApiController extends AbstractController
         $this->translator =$translator;
     }
 
-    #[Route('/order/new', methods: ['POST'])]
-    public function orderNew(Request $request): JsonResponse
-    {
-        $params  = $request->toArray();
-        $seller = $this->doctrine->getRepository(Org::class)->find($params['sellerid']);
-        $buyer = $this->doctrine->getRepository(Org::class)->find($params['buyerid']);
-        $product = $this->doctrine->getRepository(Product::class)->find($params['product']);
-        $quantity = $params['quantity'];
-        $em = $this->doctrine->getManager();
-
-        $item = new OrderItems();
-        $item->setProduct($product);
-        $item->setQuantity($quantity);
-        $em->persist($item);
-        $em->flush();
-
-        $order = new Orders();
-        $order->setSeller($seller);
-        $order->setBuyer($buyer);
-        $order->setNote($params['note']);
-        $order->addOrderItem($item);
-
-        $em->persist($order);
-        $em->flush();
-        
-        $item->setOrd($order);
-
-        $em->flush();
-
-        return $this->json([
-            'code' => 0,
-        ]);
-    }
-
     #[Route('/return/new', methods: ['POST'])]
     public function returnNew(Request $request): JsonResponse
     {
@@ -107,37 +73,6 @@ class ApiController extends AbstractController
         $em->flush();
 
         $item->setRet($ret);
-
-        $em->flush();
-
-        return $this->json([
-            'code' => 0,
-        ]);
-    }
-
-    #[Route('/retail/new', methods: ['POST'])]
-    public function retailNew(Request $request): JsonResponse
-    {
-        $params  = $request->toArray();
-        $store = $this->doctrine->getRepository(Org::class)->find($params['oid']);
-        $customer = $this->doctrine->getRepository(User::class)->find($params['cid']);
-        $product = $this->doctrine->getRepository(Product::class)->find($params['pid']);
-        $rand = $params['timestamp'];
-        $quantity = $params['quantity'];
-        $em = $this->doctrine->getManager();
-
-        $retail = new Retail();
-        $retail->setStore($store);
-        $retail->setCustomer($customer);
-        $retail->setProduct($product);
-        $retail->setQuantity($quantity);
-        $em->persist($retail);
-
-        $scan = new Scan();
-        $scan->setCustomer($customer);
-        $scan->setOrg($store);
-        $scan->setRand($rand);
-        $em->persist($scan);
 
         $em->flush();
 
@@ -175,109 +110,6 @@ class ApiController extends AbstractController
         ]);
     }
 
-    #[Route('/retail_return/new', methods: ['POST'])]
-    public function retailReturnNew(Request $request): JsonResponse
-    {
-        $params  = $request->toArray();
-        $store = $this->doctrine->getRepository(Org::class)->find($params['oid']);
-        $customer = $this->doctrine->getRepository(User::class)->find($params['cid']);
-        $product = $this->doctrine->getRepository(Product::class)->find($params['pid']);
-        $rand = $params['timestamp'];
-        $quantity = $params['quantity'];
-        $em = $this->doctrine->getManager();
-
-        $ret = new RetailReturn();
-        $ret->setStore($store);
-        $ret->setCustomer($customer);
-        $ret->setProduct($product);
-        $ret->setQuantity($quantity);
-        $em->persist($ret);
-
-        $scan = new Scan();
-        $scan->setCustomer($customer);
-        $scan->setOrg($store);
-        $scan->setRand($rand);
-        $em->persist($scan);
-
-        $em->flush();
-
-        return $this->json([
-            'code' => 0,
-        ]);
-    }
-
-    #[Route('/chpwd', methods: ['POST'])]
-    public function chpwd(Request $request, UserPasswordHasherInterface $hasher): JsonResponse
-    {
-        $params  = $request->toArray();
-        $user = $this->doctrine->getRepository(User::class)->find($params['uid']);
-        $oldPass = $params['oldPass'];
-        $plainPassword = $params['plainPassword'];
-        $em = $this->doctrine->getManager();
-
-        // if oldPass is right
-        if ($hasher->isPasswordValid($user, $oldPass)) {
-            $user->setPlainPassword($plainPassword);
-            $em->flush();
-            $code = 0;
-        } else {
-            $code = 1;
-        }
-
-        return $this->json([
-            'code' => $code,
-        ]);
-    }
-
-    #[Route('/resetpwd', methods: ['POST'])]
-    public function resetPWD(Request $request, UserPasswordHasherInterface $hasher): JsonResponse
-    {
-        $params  = $request->toArray();
-        $user = $this->doctrine->getRepository(User::class)->findOneBy(['phone' => $params['phone']]);
-        $pwd = $params['pwd1'];
-        $em = $this->doctrine->getManager();
-        $user->setPlainPassword($pwd);
-        $em->flush();
-        $code = 0;
-
-        return $this->json([
-            'code' => $code,
-        ]);
-    }
-
-    #[Route('/sms', methods: ['POST'])]
-    public function sms(Sms $sms, Request $request): JsonResponse
-    {
-        $params  = $request->toArray();
-        $phone = $params['phone'];
-        $sms->send($phone);
-        return $this->json([
-            'code' => 0,
-        ]);
-    }
-
-    #[Route('/chkotp', methods: ['POST'])]
-    public function chkotp(Request $request): JsonResponse
-    {
-        $params  = $request->toArray();
-        $phone = $params['phone'];
-        $otp = $params['otp'];
-        $cache = new RedisAdapter(RedisAdapter::createConnection('redis://localhost'));
-        // $cache = new FilesystemAdapter();
-        $otp0 = $cache->get($phone, function (ItemInterface $item){
-            return 0;
-        });
-
-        if ($otp == $otp0) {
-            $code = 0;
-        } else {
-            $code = 1;
-        }
-        return $this->json([
-            'code' => $code,
-        ]);
-    }
-
     #[Route('/refretail/{cid}', requirements: ['cid' => '\d+'],  methods: ['GET'])]
     public function refRetail(int $cid): JsonResponse
     {
@@ -293,22 +125,6 @@ class ApiController extends AbstractController
         }
         // dump($refRetails);
         return $this->json($refRetails);
-    }
-
-    #[Route('/chkphone', methods: ['POST'])]
-    public function chkPhone(Request $request): JsonResponse
-    {
-        $params  = $request->toArray();
-        $phone = $params['phone'];
-        $user = $this->doctrine->getRepository(User::class)->findOneBy(['phone' => $phone]);
-        if ($user) {
-            $code = 0;
-        } else {
-            $code = 1;
-        }
-        return $this->json([
-            'code' => $code,
-        ]);
     }
 
     #[Route('/orgs-have-stock-of-product/{pid}', methods: ['GET'])]
@@ -365,37 +181,6 @@ class ApiController extends AbstractController
         return $this->json($arr);
     }
 
-    #[Route('/pca')]
-    public function getPca(): Response
-    {
-        $pca = file_get_contents('pca.json');
-        return new Response($pca);
-    }
-    
-    #[Route('/draw', methods: ['POST'])]
-    public function draw(Request $request): JsonResponse
-    {
-        $em = $this->doctrine->getManager();
-        $params  = $request->toArray();
-        $bottleId = $params['bottle'];
-        $bottleEnc = $params['enc'];
-        $arr = explode('.', $bottleId);
-        $boxId = $arr[0];
-        $box = $this->doctrine->getRepository(Box::class)->find($boxId);
-        $boxCiphers = $box->getCipher();
-        $bottleCipher = $boxCiphers[$arr[1]];
-        $batch = $box->getBatch();
-        $prizes = $batch->getBatchPrizes();
-        dump($batch);
-        dump($prizes);
-        // https://stackoverflow.com/a/62209394/7714132
-        dump($prizes->toArray());
-        $resp = [
-            'code' => 0
-        ];
-        return $this->json($resp);
-    }
-    
     #[Route('/scan/box', methods: ['POST'])]
     public function scanBox(Request $request): Response
     {
