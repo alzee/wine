3ee3892bb343cb5aa09576b85b1e630e96fa20a2
Al Zee <z@alz.ee>
2023-03-15 14:29:35 +0800

    fields for curd settle

--- src/Controller/Admin/SettleCrudController.php
+++ src/Controller/Admin/SettleCrudController.php
@@ -3,9 +3,18 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Settle;
+use App\Entity\Choice;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DatetimeField;
 
 class SettleCrudController extends AbstractCrudController
 {
@@ -14,16 +23,20 @@ class SettleCrudController extends AbstractCrudController
         return Settle::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield IdField::new('id');
+        yield AssociationField::new('salesman');
+        yield AssociationField::new('claim');
+        yield AssociationField::new('product');
+        yield ChoiceField::new('type')
+            ->setChoices(Choice::SETTLE_TYPES)
+        ;
+        yield ChoiceField::new('status')
+            ->setChoices(Choice::SETTLE_STATUSES)
+        ;
+        yield DatetimeField::new('createdAt');
     }
-    */
     
     public function configureActions(Actions $actions): Actions
     {
