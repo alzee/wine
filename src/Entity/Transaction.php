5419a09d463696fc2d15ec512b1dffb8fd4066dd
Al Zee <z@alz.ee>
2023-04-01 14:00:52 +0800

    entity Transaction as api

--- src/Entity/Transaction.php
+++ src/Entity/Transaction.php
@@ -5,28 +5,44 @@ namespace App\Entity;
 use App\Repository\TransactionRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 
 #[ORM\Entity(repositoryClass: TransactionRepository::class)]
+#[ApiResource(
+    normalizationContext: ['groups' => ['read']],
+    denormalizationContext: ['groups' => ['write']],
+    paginationEnabled: false,
+)]
+#[ApiFilter(SearchFilter::class, properties: ['user' => 'exact', 'org' => 'exact'])]
 class Transaction
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['read'])]
     private ?int $id = null;
 
     #[ORM\ManyToOne(inversedBy: 'transactions')]
+    #[Groups(['read'])]
     private ?User $user = null;
 
     #[ORM\ManyToOne(inversedBy: 'transactions')]
+    #[Groups(['read'])]
     private ?Org $org = null;
 
     #[ORM\Column(type: Types::SMALLINT)]
+    #[Groups(['read'])]
     private ?int $type = null;
 
     #[ORM\Column]
+    #[Groups(['read'])]
     private ?int $amount = null;
 
     #[ORM\Column]
+    #[Groups(['read'])]
     private ?\DateTimeImmutable $createdAt = null;
 
     public function getId(): ?int
