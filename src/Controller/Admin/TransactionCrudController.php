1de3c0b628f5b3804ef3da58ac710fc95bc6d64e
Al Zee <z@alz.ee>
2023-04-02 20:26:06 +0800

    crud transaction fields

--- src/Controller/Admin/TransactionCrudController.php
+++ src/Controller/Admin/TransactionCrudController.php
@@ -6,6 +6,12 @@ use App\Entity\Transaction;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DatetimeField;
+use App\Entity\Choice;
 
 class TransactionCrudController extends AbstractCrudController
 {
@@ -14,16 +20,19 @@ class TransactionCrudController extends AbstractCrudController
         return Transaction::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield IdField::new('id')->onlyOnIndex();
+        yield AssociationField::new('user');
+        yield AssociationField::new('org');
+        yield ChoiceField::new('type')
+            ->setChoices(Choice::TRANSACTION_TYPES)
+        ;
+        yield MoneyField::new('amount')
+            ->setCurrency('CNY')
+        ;
+        yield DatetimeField::new('createdAt')->hideOnForm();
     }
-    */
     
     public function configureActions(Actions $actions): Actions
     {
