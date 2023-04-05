9f1b3e9b3f6ea355095552996b34dd474678e66a
Al Zee <z@alz.ee>
2023-04-05 10:18:39 +0800

    controller Wx to redirect to miniprog

---
+++ src/Controller/WxController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Service\Wx;
+
+#[Route('/wx')]
+class WxController extends AbstractController
+{
+    private $wx;
+    
+    public function __construct(Wx $wx)
+    {
+        $this->wx = $wx;
+    }
+    
+    #[Route('/cu', name: 'wx_claim_user')]
+    public function cu(): Response
+    {
+        $scheme = $this->wx->genScheme('myClaim', 'type=user');
+        return $this->render('wx/index.html.twig', [
+            'scheme' => $scheme,
+        ]);
+    }
+    
+    #[Route('/cs', name: 'wx_claim_store')]
+    public function cs(): Response
+    {
+        $scheme = $this->wx->genScheme('myClaim', 'type=user');
+        return $this->render('wx/index.html.twig', [
+            'scheme' => $scheme,
+        ]);
+    }
+}
