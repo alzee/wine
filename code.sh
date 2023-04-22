f5481d0382c9994acb9415f8815aeeeecfca32f9
Al Zee <z@alz.ee>
2023-04-22 11:43:24 +0800

    nth

--- code.sh
+++ code.sh
@@ -8,6 +8,6 @@ echo -e // 后端\\n > $file
 find src/ -type f -exec echo // {} >> $file \; -exec cat {} >> $file  \; -exec echo >> $file \;
 
 echo -e // 前端\\n >> $file
-find ~/w/taro.wine/src/ -type f ! -name *.png -exec echo // {} >> $file \; -exec cat {} >> $file  \; -exec echo >> $file \;
+find ~/w/taro.jiu/src/ -type f ! -name *.png -exec echo // {} >> $file \; -exec cat {} >> $file  \; -exec echo >> $file \;
 
 [ -d public/ ] && mv $file public/
